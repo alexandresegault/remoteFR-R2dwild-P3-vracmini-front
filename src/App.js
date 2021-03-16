23c8b2331a2e3b784f2ad75378ab9e209c715955
alexandresegault <alexandresegault@gmail.com>
2021-03-16 15:39:02 +0100

    css amelioré

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css'
 import Header from './components/Header'
+import AdminInterfaceAccueil from './admin/AdminInterfaceAccueil.js'
+import { Switch, Route } from 'react-router-dom'
+
+import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <Header />
+      <Switch>
+        <Route exact path='/' component={Header} />
+        <Route path='/admin' component={AdminInterfaceAccueil} />
+      </Switch>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,18 @@ import React from 'react'
 import './Header.css'
 import NavBar from '../components/NavBar'
 
+import user from '../img/user.png'
+import { Link } from 'react-router-dom'
+
 export default function Header() {
   return (
     <div className='header'>
       <NavBar />
+      <div>
+        <Link to='/admin'>
+          <img src={user} alt='connexion logo' />
+        </Link>
+      </div>
     </div>
   )
 }
