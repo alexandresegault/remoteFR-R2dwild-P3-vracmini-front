8e9d4051eb058c205620c75c3d7e2dcbce0a00bd
alexandresegault <alexandresegault@gmail.com>
2021-03-17 12:38:09 +0100

    ajout du lien vers interface admin

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 import Search from './Search'
 import Burger_menu from './Burger_menu'
 import user from '../img/userGreen.png'
-
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className='header'>
@@ -14,7 +14,9 @@ const Header = () => {
       <h1 className='title-website'>
         <span className='yellow'>m</span>ini<span className='yellow'>m</span>al
       </h1>
-      <img src={user} alt='' className='user-logo' />
+      <Link to='/admin'>
+        <img src={user} alt='' className='user-logo' />
+      </Link>
     </div>
   )
 }
