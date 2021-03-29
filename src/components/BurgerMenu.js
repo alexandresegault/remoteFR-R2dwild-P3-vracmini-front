71352262c4a493ddae1d9f0c7d3e3464243317e8
RaphNokos <raph.nokos@outlook.com>
2021-03-29 09:56:46 +0200

    css burger menu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,13 +1,6 @@
 import { NavLink, Link } from 'react-router-dom'
 
 import './BurgerMenu.css'
-// import imgEnVrac from '../img/histoireEnVrac.png'
-// import imgVracons from '../img/ensembleVracons.png'
-// import imgEshop from '../img/e-shop.png'
-// import imgCommunity from '../img/vracCommunity.png'
-// import imgVracNCo from '../img/vracNCo.png'
-// import imgFourneaux from '../img/auFourneaux.png'
-// import imgContact from '../img/contact.png'
 import imgWoman from '../img/personnage1Reduit.png'
 
 const BurgerMenu = prevProps => {
@@ -16,17 +9,6 @@ const BurgerMenu = prevProps => {
       <div id='menu-page' className={prevProps.burgerContent}>
         <div className='menu-global'>
           <img className='woman-burger' src={imgWoman} alt='femme' />
-          {/* <div className='menu-burger-link'>
-          <img className='img-burger' src={imgEnVrac} alt='links' />
-          <img className='img-burger' src={imgVracons} alt='links' />
-          <Link to='/aux_fourneaux'>
-            <img className='img-burger' src={imgFourneaux} alt='liens' />
-          </Link>
-          <img className='img-burger' src={imgVracNCo} alt='liens' />
-          <img className='img-burger' src={imgContact} alt='liens' />
-          <img className='img-burger' src={imgEshop} alt='links' />
-          <img className='img-burger' src={imgCommunity} alt='links' />
-        </div> */}
           <ul className='menu-list'>
             <li className='menu-link'>
               <NavLink
