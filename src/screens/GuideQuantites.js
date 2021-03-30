5e1b61e52bdd086aaa23e01929f2f606e9482fed
RaphNokos <raph.nokos@outlook.com>
2021-03-30 23:12:54 +0200

    page quantités ok

--- src/screens/GuideQuantites.js
+++ src/screens/GuideQuantites.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import './GuideQuantites.css'
 
 const GuideQuantites = () => {
-
   const [imgIntro1] = useState(
     'https://drive.google.com/uc?id=1hxpf6JEKj1HuDwvcU8KuWcpvPIb-jIYL'
   )
@@ -20,13 +20,111 @@ const GuideQuantites = () => {
   const [imgIntro6] = useState(
     'https://drive.google.com/uc?id=1gnzqqAmrmNByyTgc8WY-bUtjq0cnZTFO'
   )
+  const [imgQuantite1] = useState(
+    'https://drive.google.com/uc?id=1y8X8Gk8hJSO33Iw8G4E2KdSfOwVyMxBk'
+  )
+  const [imgQuantites4] = useState(
+    'https://drive.google.com/uc?id=1Sm_qv_bMqRc3Z1JFNs9dt4rVzgd-bUCc'
+  )
 
-  return(
+  return (
     <div>
-      <div>
-        <img className="pot-quantites-intro" src={imgIntro1} alt="texte intro quantités" />
-        <img className="pot-quantites-intro" src={imgIntro2} alt="texte intro quantités" />
-        <img className="pot-quantites-intro" src={imgIntro3} alt="texte intro quantités" />
+      <div className='pots-position'>
+        <img
+          className='pot-quantites-intro'
+          src={imgIntro1}
+          alt='texte intro quantités'
+        />
+        <img
+          className='pot-quantites-intro'
+          src={imgIntro2}
+          alt='texte intro quantités'
+        />
+        <img
+          className='pot-quantites-intro'
+          src={imgIntro3}
+          alt='texte intro quantités'
+        />
+      </div>
+      <h2 className='text-intro-quantites'>
+        Résultat, la quantité préparée est rarement la bonne...
+      </h2>
+      <div className='pots-position'>
+        <img
+          className='pot-quantites-intro'
+          src={imgIntro4}
+          alt='texte intro quantités'
+        />
+        <img
+          className='pot-quantites-intro'
+          src={imgIntro5}
+          alt='texte intro quantités'
+        />
+        <img
+          className='pot-quantites-intro'
+          src={imgIntro6}
+          alt='texte intro quantités'
+        />
+      </div>
+      <div className='indications-global'>
+        <div className='indications-quantites'>
+          <img
+            className='pot-x'
+            src={imgQuantite1}
+            alt='texte intro quantités'
+          />
+          <div className='name-aliments'>
+            <p>Pâtes coquillette</p>
+            <p>Lentille</p>
+            <p>Quinoa</p>
+            <p>Boulgour</p>
+            <p>Pois cassés</p>
+            <p>Semoule</p>
+            <p>Riz</p>
+            <p>Pois chiches</p>
+          </div>
+          <div className='indications-grammes'>
+            <p>100 grs</p>
+            <p>60 grs</p>
+            <p>70 grs</p>
+            <p>80 grs</p>
+            <p>60 grs</p>
+            <p>80 grs</p>
+            <p>50 grs</p>
+            <p>70 grs</p>
+          </div>
+        </div>
+        <div className='indications-quantites'>
+          <img
+            className='pot-x'
+            src={imgQuantites4}
+            alt='texte intro quantités'
+          />
+          <div className='name-aliments'>
+            <p>Pâtes coquillette</p>
+            <p>Lentille</p>
+            <p>Quinoa</p>
+            <p>Boulgour</p>
+            <p>Pois cassés</p>
+            <p>Semoule</p>
+            <p>Riz</p>
+            <p>Pois chiches</p>
+          </div>
+          <div className='indications-grammes'>
+            <p>500 grs</p>
+            <p>400 grs</p>
+            <p>200 grs</p>
+            <p>180 grs</p>
+            <p>250 grs</p>
+            <p>200 grs</p>
+            <p>200 grs</p>
+            <p>500 grs</p>
+          </div>
+        </div>
+      </div>
+      <div className='infos-indications-global'>
+      <div className='infos-indications'>Quantités indicatives conseillées pour un repas, en plat principal.</div>
+      <div className='infos-indications'>Quantités indicatives conseillées pour un repas, en plat principal.</div>
       </div>
     </div>
   )
