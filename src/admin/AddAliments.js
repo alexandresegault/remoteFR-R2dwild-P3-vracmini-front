05fe8d2f56214646559f6f3f2016f1b842d555d6
alexandresegault <alexandresegault@gmail.com>
2021-04-09 09:49:11 +0200

    ok

--- src/admin/AddAliments.js
+++ src/admin/AddAliments.js
@@ -30,61 +30,58 @@ const AddAliments = () => {
 
   return (
     <div className='interface-add-aliments'>
-      <div>
-        <label>Nom aliment :</label>
-
-        <input
-          type='text'
-          id='name-categorie-input'
-          placeholder='Pois chiche'
-          name='name'
-          onChange={event => setName(event.target.value)}
-        />
-        <label>Titre :</label>
-        <input
-          type='text'
-          id='title-input'
-          placeholder='Meet Substitute'
-          name='title'
-          onChange={event => setTitle(event.target.value)}
-        />
-        <label>URL image :</label>
-        <input
-          type='text'
-          id='urlimg-input'
-          placeholder='http..'
-          name='urlimg'
-          onChange={event => setUrlImg(event.target.value)}
-        />
-        <div className='editor'>
-          <Editor
-            apiKey={ApiKey}
-            onChange={handleEditorChange}
-            id='thisContent'
-            init={{
-              height: 500,
-              menubar: true,
-              quickbars_image_toolbar:
-                'alignleft aligncenter alignright | rotateleft rotateright | imageoptions',
-              plugins: [
-                'advlist autolink lists link image',
-                'charmap print preview anchor help',
-                'searchreplace visualblocks code',
-                'a_tinymce_plugin',
-                'insertdatetime media table paste wordcount'
-              ],
-              toolbar:
-                'undo redo | formatselect | bold italic | \
+      <label>Nom aliment :</label>
+      <input
+        type='text'
+        id='name-categorie-input'
+        placeholder='Pois chiche'
+        name='name'
+        onChange={event => setName(event.target.value)}
+      />
+      <label>Titre :</label>
+      <input
+        type='text'
+        id='title-input'
+        placeholder='Meet Substitute'
+        name='title'
+        onChange={event => setTitle(event.target.value)}
+      />
+      <label>URL image :</label>
+      <input
+        type='text'
+        id='urlimg-input'
+        placeholder='http..'
+        name='urlimg'
+        onChange={event => setUrlImg(event.target.value)}
+      />
+      <label>Contenu :</label>
+      <div className='editor'>
+        <Editor
+          apiKey={ApiKey}
+          onChange={handleEditorChange}
+          id='thisContent'
+          init={{
+            height: 500,
+            menubar: true,
+            quickbars_image_toolbar:
+              'alignleft aligncenter alignright | rotateleft rotateright | imageoptions',
+            plugins: [
+              'advlist autolink lists link image',
+              'charmap print preview anchor help',
+              'searchreplace visualblocks code',
+              'a_tinymce_plugin',
+              'insertdatetime media table paste wordcount'
+            ],
+            toolbar:
+              'undo redo | formatselect | bold italic | \
               alignleft aligncenter alignright | \
               bullist numlist outdent indent | help'
-            }}
-          />
-        </div>
-        <button className='send-button' onClick={() => send()}>
-          Envoyer
-        </button>
+          }}
+        />
       </div>
-      <div className='final'></div>
+      <button className='send-button' onClick={() => send()}>
+        Envoyer
+      </button>
     </div>
   )
 }
