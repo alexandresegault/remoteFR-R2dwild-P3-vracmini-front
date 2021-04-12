63dada4eb6fa135d8a438c28863db11bf92f780a
alexandresegault <alexandresegault@gmail.com>
2021-04-12 10:21:44 +0200

    ok

--- src/admin/InterfaceArticles.js
+++ src/admin/InterfaceArticles.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
 import './InterfaceArticles.css'
 
 const InterfaceArticles = () => {
+  const [listPodart, setListPodart] = useState([])
+  const [categoriePodart, setCategoriePodart] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4242/api/podcasts_articles').then(res => {
+      setListPodart(res.data)
+      console.log('premier' + res.data)
+    })
+  }, [])
+  useEffect(() => {
+    axios
+      .get('http://localhost:4242/api/categories_podcasts_articles')
+      .then(res => {
+        console.log('deuxieme' + res.data)
+        setCategoriePodart(res.data)
+      })
+  }, [])
   return (
     <div className='interface-articles'>
       <div></div>
